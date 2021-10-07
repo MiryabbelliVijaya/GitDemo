5d58fe69b2fbbc326e438174dcd1a6e13653a203
Vijaya <miryabbellivijaya@gmail.com>
2021-10-07 05:29:19 -0400

    Vijaya1 class added

---
+++ src/test/java/EBanking/EBanking/Vijaya1.java
@@ -0,0 +1,10 @@
+package EBanking.EBanking;
+
+public class Vijaya1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" This is Vijaya1");
+	}
+
+}
